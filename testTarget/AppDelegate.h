26d1d159219dffb804d45af2bacd06385f9dab95
Harvey Yang <harveyMac@YangdeMBP.lan>
2016-03-07 17:07:28 +0800

    1

---
+++ testTarget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testTarget
+//
+//  Created by Harvey Yang on 16/3/7.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testTarget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testTarget
+//
+//  Created by Harvey Yang on 16/3/7.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
